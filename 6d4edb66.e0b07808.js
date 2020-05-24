939a08319d6ecbb13004f66e2814f7d9d2cc5366
Afshin Mehrabani <afshin.meh@gmail.com>
2020-05-24 19:54:35 +0100

    Deploy website - based on 36ecac4e4a2688ac1cb6b8da47638f1c8056bc6f

---
+++ 6d4edb66.e0b07808.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{114:function(n){n.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"announcements","name":"announcements","count":1,"permalink":"/blog/tags/announcements"}')}}]);
