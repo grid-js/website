ae4615ce2e5ae5ce464b5cbf5a1c983ad60a10b9
Salama Ashoush <salamaashoush@gmail.com>
2020-09-10 15:57:55 +0200

    fix: add angular page to sidebar

--- sidebars.js
+++ sidebars.js
@@ -31,6 +31,7 @@ module.exports = {
     "🔌 Integrations": [
       'integrations/react',
       'integrations/vue',
+      'integrations/angular',
       'integrations/jquery',
     ],
     "🎮 Examples": [{
